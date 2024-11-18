95dcb71072f8650cb6ead760de0e52888bbdbc0e
maiconstolpho <105317762+maiconstolpho@users.noreply.github.com>
2024-11-18 10:17:00 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,70 @@
+criaCartao(
+  " Cultivo Do Feijão ",
+  "Qual é a época ideal para plantar feijão?",
+  "A época ideal varia conforme a região, mas geralmente é na primavera ou no início do verão, quando as temperaturas estão mais amenas."
+);
+
+criaCartao(
+  "Cultivo Do Feijão ",
+  "Qual tipo de solo é melhor para o cultivo de feijão?",
+  "O feijão prefere solos bem drenados, ricos em matéria orgânica e com pH entre 6 e 7"
+);
+
+criaCartao(
+  "Cultivo Do Feijão ",
+  "Qual é a distância ideal entre as plantas de feijão?",
+  "O recomendado é um espaçamento de 30 a 40 cm entre as plantas, para garantir boa circulação de ar e luz."
+);
+
+criaCartao(
+  "Cultivo Do Feijão ",
+  "Qual é o tempo médio de colheita do feijão?",
+  "Entre 60 a 90 dias, dependendo da variedade."
+);
+
+criaCartao(
+  "Cultivo Do Feijão ",
+  "O feijão precisa de fertilizantes?",
+  "Sim, fertilizantes ricos em nitrogênio e fósforo são recomendados."
+);
+
+criaCartao(
+  "Cultivo Do Feijão ",
+  "Como saber quando o feijão está maduro para colheita?",
+  "Quando as vagens estão secas e duras."
+);
+
+criaCartao(
+  "Cultivo Do Feijão ",
+  "Quais são os principais tipos de feijão cultivados na roça?",
+  "Feijão preto, carioca e vermelho."
+);
+
+criaCartao(
+  "Cultivo Do Feijão ",
+  "Como o feijão contribui para a fertilidade do solo?",
+  "Ele fixa nitrogênio no solo, melhorando sua qualidade."
+);
+
+criaCartao(
+  "Cultivo Do Feijão ",
+  "Qual é a temperatura ideal para germinação das sementes?",
+  "Entre 20 e 30 °C."
+);
+criaCartao(
+  "Cultivo Do trigo ",
+  " Quais são as principais condições climáticas necessárias para o cultivo do trigo?",
+  "O trigo se desenvolve melhor em climas temperados, com temperaturas ideais entre 15°C e 24°C. O solo deve ser bem drenado, e a planta requer uma quantidade adequada de chuvas, geralmente entre 400 a 600 mm durante o ciclo de cultivo."
+);
+
+criaCartao(
+  "Cultivo Do trigo",
+  "Quais são as principais pragas e doenças que afetam a cultura do trigo?",
+  " Entre as principais pragas estão a lagarta-da-espiga e o pulgão, que podem causar danos significativos às plantações. As doenças mais comuns incluem a ferrugem, a brusone e a mancha foliar, que podem comprometer a produtividade se não forem controladas adequadamente."
+);
+
+criaCartao(
+  "Cultivo Do trigo",
+  " Qual é a importância econômica do trigo?",
+  "O trigo é uma das principais culturas alimentares do mundo, sendo a base para a produção de farinha, pães, massas e outros produtos. Sua importância econômica é significativa, pois é uma fonte essencial de carboidratos e proteínas, além de representar uma fonte de renda para milhões de agricultores em diversas regiões."
+);
